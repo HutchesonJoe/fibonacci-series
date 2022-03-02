0777e34e25e694cc3be0c2ee774dbcd3acc396fd
Joe <hutcheson2001@yahoo.com>
2022-03-02 18:37:21 -0500

    completed assignment

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -1,9 +1,20 @@
 function fibonacci(num) {
-  // type your code here
+  let arr = [0, 1]
+  for (let i = 0; i<num; i++){
+    arr.push(arr[i] + arr[i+1]);
+    console.log(arr)
+  }
+  if (i = 0){
+    return 0
+  }
+  return arr[num]
 }
 
 if (require.main === module) {
-  // add your own tests in here
+  console.log("Expecting: 196418");
+  console.log("=>", fibonacci(27));
+
+  console.log("");
   console.log("Expecting: 0");
   console.log("=>", fibonacci(0));
 
@@ -20,5 +31,4 @@ if (require.main === module) {
 
 module.exports = fibonacci;
 
-// Please add your pseudocode to this file
-// And a written explanation of your solution
+// This function takes in a number, and finds that numth element in the Finonacci series. First, I need to create the series, which is a series that starts with [0, 1...] continueing with the next number being the sum of the two previous numbers. 
